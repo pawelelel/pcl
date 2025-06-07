d56ee39e9215210693ff3be332baaec1bc2da8b0
PAWEL\pawci <pawel.leczkowski2005@gmail.com>
2025-06-07 14:46:15 +0200

    Title and console dimentions

--- pcl/pcl.c
+++ pcl/pcl.c
@@ -2,8 +2,81 @@
 // Created by pawci on 14.05.2025.
 //
 
+#include <pcl.h>
 #include <stdio.h>
+#include <windows.h>
 
-void hello(void) {
-	puts("Hello World!");
+struct Console* Start(void) {
+	struct Console* console = malloc(sizeof(struct Console));
+	console->inputHandle = GetStdHandle(STD_INPUT_HANDLE);
+	console->outputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	console->errorHandle = GetStdHandle(STD_ERROR_HANDLE);
+	console->windowHandle = GetCurrentProcess();
+	return console;
+}
+
+/**
+ * Ends a console window
+ *
+ * @param console pointer to struct Console
+ */
+void End(struct Console* console) {
+	CloseHandle(console->inputHandle);
+	CloseHandle(console->outputHandle);
+	CloseHandle(console->errorHandle);
+	free(console);
+}
+
+/**
+ * @returns char* a pointer to name of console
+ */
+char* GetTitle(struct Console* console) {
+	char* title = malloc(MAX_PATH * sizeof(char));
+	//GetWindowTextA(console->windowHandle, title, MAX_PATH);
+	GetConsoleTitleA(title, MAX_PATH);
+	return title;
+}
+
+/**
+ * Sets a new title to console window
+ *
+ * @returns 1 if eveything went well otherwise returns 0
+ */
+int SetTitle(struct Console* console, const char* title) {
+	//if(!SetWindowTextA(console->windowHandle, title))
+	if(!SetConsoleTitleA(title))
+	{
+		return 0;
+	}
+	return 1;
+}
+
+int GetConsoleDimentions(const struct Console* console, int* width, int* height) {
+	if (width == NULL) {
+		return -1;
+	}
+	if (height == NULL) {
+		return -2;
+	}
+	if (console == NULL) {
+		*width = 0;
+		*height = 0;
+		return -3;
+	}
+	if (console->outputHandle == INVALID_HANDLE_VALUE) {
+		*width = 0;
+		*height = 0;
+		return -4;
+	}
+
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	if (GetConsoleScreenBufferInfo(console->outputHandle, &info) == 0) {
+		*width = 0;
+		*height = 0;
+		return -5;
+	}
+
+	*width = info.dwSize.X;
+	*height = info.dwSize.Y;
+	return 0;
 }
--- pcl/pcl.h
+++ pcl/pcl.h
@@ -2,9 +2,26 @@
 // Created by pawci on 14.05.2025.
 //
 
+#include <bemapiset.h>
+#include <windows.h>
 #ifndef PCL_H
 
-void hello(void);
+struct Console {
+	HANDLE inputHandle;
+	HANDLE outputHandle;
+	HANDLE errorHandle;
+	HANDLE windowHandle;
+};
+
+struct Console* Start(void);
+
+void End(struct Console* console);
+
+char* GetTitle(struct Console* console);
+
+int SetTitle(struct Console* console, const char* title);
+
+int GetConsoleDimentions(const struct Console* console, int* width, int* height);
 
 #define PCL_H
 
